ae6d55478fd9242802bdc72e4dd1a241cde2e0da
cunhasb CUNHA <cunhasb@aol.com>
2017-10-02 23:23:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,16 @@ def countdown(number)
   while counter>= 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  counter = number
+
+  while counter>= 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
     sleep 1
   end
   "HAPPY NEW YEAR!"
